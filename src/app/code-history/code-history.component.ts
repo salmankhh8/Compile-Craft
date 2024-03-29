13c43484e2ba9824f7297708eac057aa093d58b7
salmankhh8 <salmankhh8salmankhh8@users.noreply.github.com>
2024-03-29 22:53:33 +0530

    routes cehck

--- src/app/code-history/code-history.component.ts
+++ src/app/code-history/code-history.component.ts
@@ -372,9 +372,8 @@ export class CodeHistoryComponent implements AfterViewInit, OnInit {
   }
 
   openInNewTab(router:string,id:string){
+    console.log(window.location, router,"Window Location");
     let currentHref = window.location.origin + `/${router}/${id}`
-    console.log(window.location, "Window Location");
-    
     window.open(currentHref)
   }
 
